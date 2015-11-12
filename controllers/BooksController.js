c3710957349280be6f912f5796d520a4d674bf4d
Artem Linenko <artem.linenko@globallogic.com>
2015-11-12 13:33:14 +0200

    Fetching books list from nongoLab

--- controllers/BooksController.js
+++ controllers/BooksController.js
@@ -1,17 +1,17 @@
-// var booksService = require('../services/booksService.js')
 
 export default function(app) {
-	return app.controller('BooksController', function($scope, $rootScope){
-		// booksService.getBooks.success(function(data) {
-		// 	$scope.books = data;
-		// })
-		$scope.books = require('../books.json');
-		$scope.books.forEach((book) => {
-			book.amount = 1;
-		});
+	var booksService = require('../services/booksService.js')(app);
+	return app.controller('BooksController', ['$scope', '$rootScope', 'booksService', function($scope, $rootScope, booksService){
+		// Fetching list from mingoLab
+		booksService.getBooks().then(function(data) {
+			$scope.books = data.data;
+			$scope.books.forEach((book) => {
+				book.amount = 1;
+			});
+		})
 
 		$scope.addItem = function(book) {
 			$rootScope.$broadcast('itemAdded', book);
 		}
-	})
+	}])
 }
--- services/booksService.js
+++ services/booksService.js
@@ -1,25 +1,29 @@
-var apiKey = mimDIGUBZh5cp3i56VnHTcrdCIVL1rKC;
+var apiKey = 'mimDIGUBZh5cp3i56VnHTcrdCIVL1rKC',
+	baseUrl = "https://api.mongolab.com/api/1/databases/angular_cart_app/collections/books";
 
 
-app.factory('booksService', ["$http", function($http) {
-	return {
-		getBooks: function() {
-			return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/shows-api/shows.json')
-			  .success(function(data) {
-			  	return data
-			  })
-			  error(function(err) {
-			  	return err
-			  })
-		},
-		postBook: function() {
-			return $http.post('https://s3.amazonaws.com/codecademy-content/courses/ltp4/shows-api/shows.json')
-			  .success(function(data) {
-			  	return data
-			  })
-			  error(function(err) {
-			  	return err
-			  })
+export default function(app) {
+	return app.factory('booksService', ["$http", function($http) {
+		return {
+			getBooks: function() {
+				return $http.get(baseUrl, {params: {apiKey: apiKey}})
+				  .success(function(data) {
+				  	console.log(data)
+				  	return data
+				  })
+				  error(function(err) {
+				  	return err
+				  })
+			},
+			postBook: function() {
+				return $http.post(baseUrl, {params: {apiKey: apiKey}})
+				  .success(function(data) {
+				  	return data
+				  })
+				  error(function(err) {
+				  	return err
+				  })
+			}
 		}
-	}
-}])
+	}])
+}
