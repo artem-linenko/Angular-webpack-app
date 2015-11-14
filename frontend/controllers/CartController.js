24e42f49ca4498b9eb15a77a94f44c32405e7190
Artem Linenko <artem.linenko@globallogic.com>
2015-11-14 17:44:02 +0200

    Added total and removing item

--- frontend/controllers/CartController.js
+++ frontend/controllers/CartController.js
@@ -1,8 +1,21 @@
 export default function(app) {
 	return app.controller('CartController', function($scope){
 		$scope.cart = [];
-
 		$scope.$on('itemAdded', addItem);
+
+		$scope.removeItem = function(index) {
+			$scope.cart.splice(index, 1);
+
+			$scope.countTotal()
+		}
+
+		$scope.countTotal = function() {
+			$scope.total = 0;
+
+			$scope.cart.forEach((book) => {
+				$scope.total += book.price * book.amount
+			})
+		}
 
 		function addItem (e, newBook) {
 			let index = $scope.cart.findIndex((book) => book.id === newBook.id);
@@ -12,6 +25,8 @@ export default function(app) {
 			} else {
 				$scope.cart.push(cloneObj(newBook));
 			}
+
+			$scope.countTotal();
 		}
 
 		function cloneObj(obj) {
